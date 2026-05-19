d1b790659e4d6798da1c5520867c7b064ef2c104
okitha_eske <164752460+okithaeske@users.noreply.github.com>
2026-05-19 12:17:35 +0530

    Adding initial Factory method to push for git repo

--- src/player/PlayerFactory.java
+++ src/player/PlayerFactory.java
@@ -1,4 +1,28 @@
 package player;
 
+import enums.Colour;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerFactory {
+
+    public static List<AbstractPlayer> createPlayers() {
+        List<AbstractPlayer> players = new ArrayList<>();
+        players.add(createPlayer(Colour.RED));
+        players.add(createPlayer(Colour.GREEN));
+        players.add(createPlayer(Colour.YELLOW));
+        players.add(createPlayer(Colour.BLUE));
+        return players;
+    }
+
+    public static AbstractPlayer createPlayer(Colour colour) {
+        return switch (colour) {
+            case RED -> new RedPlayer();
+            case GREEN -> new GreenPlayer();
+            case YELLOW -> new YellowPlayer();
+            case BLUE -> new BluePlayer();
+            default -> throw new IllegalArgumentException("Unknown colour: " + colour);
+        };
+    }
 }
